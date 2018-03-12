a6fa8137471c9a2d84b194c1d4978f797133e2ca
avinash <avish1990@gmail.com>
2018-03-12 13:21:11 +0000

    modified script

--- sam-ssm/instance.sh
+++ sam-ssm/instance.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-aws ec2 describe-instances --instance-ids  | grep -i instanceId | awk '{print$2}' | sed -e 's/\"//g' | sed -e 's/,//g' > ./instance.txt
+ssh jenkins@10.0.1.2 "aws ec2 describe-instances --instance-ids  | grep -i instanceId | awk '{print$2}' | sed -e 's/\"//g' | sed -e 's/,//g'" > ./instance.txt
