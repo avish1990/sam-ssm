16e2b1b3d960cd5bd78ed0d213e5200b77e7de99
avinash <avish1990@gmail.com>
2018-03-12 14:18:35 +0000

    create function1

--- sam-ssm/instance.sh
+++ sam-ssm/instance.sh
@@ -5,4 +5,4 @@
 
 ssh jenkins@10.0.1.2 aws cloudformation package --template-file example.yaml --output-template-file sam-ssm.yaml --s3-bucket snslambssm
 
-aws cloudformation deploy --template-file sam-ssm.yaml --stack-name sam-ssm-lam-deploy --capabilities CAPABILITY_IAM
+ssh jenkins@10.0.1.2 aws cloudformation deploy --template-file sam-ssm.yaml --stack-name sam-ssm-lam-deploy --capabilities CAPABILITY_IAM
