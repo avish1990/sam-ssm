5778e25decec58d61961ea9adc35fb935737428c
avish1990 <avish1990@gmail.com>
2018-03-12 20:00:46 +0530

    Update instance.sh

--- sam-ssm/instance.sh
+++ sam-ssm/instance.sh
@@ -3,6 +3,7 @@
 #ssh jenkins@10.0.1.2 aws ec2 describe-instances --instance-ids  | grep -i instanceId | awk '{print$2}' | sed -e 's/\"//g' | sed -e 's/,//g' > ./instance.txt
 
 cd sam-ssm
+ls -lrt
 
 ssh jenkins@10.0.1.2 aws cloudformation package --template-file example.yaml --output-template-file sam-ssm.yaml --s3-bucket snslambssm
 
