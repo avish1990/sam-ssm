38456269069ed8306c97b0608f95faf64807be94
avinash <avish1990@gmail.com>
2018-03-13 10:17:55 +0000

    Revert "script modified for all instances"
    This reverts commit ec13fabb1d1f15aed801cf00a60c9ff4711a0c90.
    
    revertinh

--- sam-ssm/index.py
+++ sam-ssm/index.py
@@ -1,23 +1,19 @@
 import boto3
 import time
 import json
-import sys
 
 def lambda_handler(event, context):
-
+    
     ssm = boto3.client('ssm')
     message = event['Records'][0]['Sns']['Message']
+    instanceID = 'i-077cba0abb9f7d184'
     documentName = 'AWS-RunShellScript'
     commandopen = ['iptables -I INPUT -p tcp --dport 8080 -j ACCEPT']
     commandclose  = ['iptables -I INPUT -p tcp --dport 8080 -j DROP']
-    ec2 = boto3.client('ec2')
-    response = ec2.describe_instances()
-    for r in response['Reservations']:
-      for instance in r['Instances']:  
-	
-        if message.lower() == 'start':
-          status = ssm.send_command(DocumentName=documentName, TimeoutSeconds=timeout, Parameters={'commands': commandopen}, InstanceIds=[instance])
-        elif message.lower() == 'stop':
-          status = ssm.send_command(DocumentName=documentName, TimeoutSeconds=timeout, Parameters={'commands': commandclose}, InstanceIds=[instance])
-        else:
-          print('Invalid Input')
+
+    if message.lower() == 'start':
+        status = ssm.send_command(DocumentName=documentName, TimeoutSeconds=timeout, Parameters={'commands': commandopen}, InstanceIds=[instanceID])
+    elif message.lower() == 'stop':
+        status = ssm.send_command(DocumentName=documentName, TimeoutSeconds=timeout, Parameters={'commands': commandclose}, InstanceIds=[instanceID])
+    else:
+        print('Invalid Input')
