338ef471df18f2ea6b208e591fefb115475d4698
avish1990 <avish1990@gmail.com>
2018-03-18 16:41:19 +0530

    Update index.py

--- sam-ssm/index.py
+++ sam-ssm/index.py
@@ -7,17 +7,11 @@ def handler(event, context):
 
     ssm = boto3.client('ssm')
     message = event['Records'][0]['Sns']['Message']
-    documentName = 'AWS-RunShellScript'
-    commandopen = ['iptables -I INPUT -p tcp --dport 8080 -j ACCEPT']
-    commandclose  = ['iptables -I INPUT -p tcp --dport 8080 -j DROP']
+    documentName = 'avish-port'
     ec2 = boto3.client('ec2')
     response = ec2.describe_instances()
     for r in response['Reservations']:
-      for instance in r['Instances']:  
-        instanceid = instance['InstanceId'] 	
-        if message.lower() == 'start':
-          status = ssm.send_command(DocumentName=documentName, Parameters={'commands': commandopen}, InstanceIds=[instanceid])
-        elif message.lower() == 'stop':
-          status = ssm.send_command(DocumentName=documentName, Parameters={'commands': commandclose}, InstanceIds=[instanceid])
-        else:
-          print('Invalid Input')
+      for instance in r['Instances']:
+        instanceid = instance['InstanceId']
+        status = ssm.send_command(DocumentName=documentName,  Parameters={'Rule': [message.upper()]},  InstanceIds=[instanceid])
+        
