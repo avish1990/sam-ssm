b131f5aa590332078c65838a1534a2143cf2523f
avish1990 <avish1990@gmail.com>
2018-03-19 15:53:44 +0530

    using tags instead of instance ids

--- sam-ssm/index.py
+++ sam-ssm/index.py
@@ -4,9 +4,4 @@ def handler(event, context):
     ssm = boto3.client('ssm')
     message = event['Records'][0]['Sns']['Message']
     documentName = 'avish-port'
-    ec2 = boto3.client('ec2')
-    response = ec2.describe_instances()
-    for r in response['Reservations']:
-      for instance in r['Instances']:
-        instanceid = instance['InstanceId']
-        status = ssm.send_command(DocumentName=documentName,  Parameters={'Rule': [message.upper()]},  InstanceIds=[instanceid])
+    status = ssm.send_command(DocumentName=documentName,  Parameters={'Rule': [message.upper()]},  Targets=[{'Key': 'tag:env','Values': ['dev']}]))
