6d657dea9f4f68503b7da536fdd6ec7f3ba99847
avish1990 <avish1990@gmail.com>
2018-03-19 11:52:50 +0530

    Latest script

--- sam-ssm/index.py
+++ sam-ssm/index.py
@@ -3,21 +3,14 @@ import time
 import json
 import sys
 
-def lambda_handler(event, context):
+def handler(event, context):
 
     ssm = boto3.client('ssm')
     message = event['Records'][0]['Sns']['Message']
-    documentName = 'AWS-RunShellScript'
-    commandopen = ['iptables -I INPUT -p tcp --dport 8080 -j ACCEPT']
-    commandclose  = ['iptables -I INPUT -p tcp --dport 8080 -j DROP']
-	ec2 = boto3.client('ec2')
+    documentName = 'avish-port'
+    ec2 = boto3.client('ec2')
     response = ec2.describe_instances()
     for r in response['Reservations']:
-      for instance in r['Instances']:  
-	
-        if message.lower() == 'start':
-          status = ssm.send_command(DocumentName=documentName, TimeoutSeconds=timeout, Parameters={'commands': commandopen}, InstanceIds=[instance])
-        elif message.lower() == 'stop':
-          status = ssm.send_command(DocumentName=documentName, TimeoutSeconds=timeout, Parameters={'commands': commandclose}, InstanceIds=[instance])
-        else:
-          print('Invalid Input')
+      for instance in r['Instances']:
+        instanceid = instance['InstanceId']
+        status = ssm.send_command(DocumentName=documentName,  Parameters={'Rule': [message.upper()]},  InstanceIds=[instanceid])
