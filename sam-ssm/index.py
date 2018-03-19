e3a12a770ecadf39295d50824c111b9a596b49ee
avish1990 <avish1990@gmail.com>
2018-03-19 16:13:08 +0530

    Update index.py

--- sam-ssm/index.py
+++ sam-ssm/index.py
@@ -4,4 +4,4 @@ def handler(event, context):
     ssm = boto3.client('ssm')
     message = event['Records'][0]['Sns']['Message']
     documentName = 'avish-port'
-    status = ssm.send_command(DocumentName=documentName,  Parameters={'Rule': [message.upper()]},  Targets=[{'Key': 'tag:env','Values': ['dev']}]))
+    response=ssm.send_command(DocumentName=documentName,  Parameters={'Rule': [message.upper()]},  Targets=[{'Key': 'tag:env','Values': ['dev']}])
