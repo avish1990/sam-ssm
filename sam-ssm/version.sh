304ff8486369b0ad28c7803d7af0782678ac27d1
avish1990 <avish1990@gmail.com>
2018-03-19 19:12:37 +0530

    Create version.sh

---
+++ sam-ssm/version.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+version=`aws lambda get-alias --name prod --function-name SSM | grep FunctionVersion | sed -e 's/\"//g' |  awk '{print$2}' | sed -e 's/,//g'`
+prevversion=`expr $version - 1`
+echo "$prevversion"
