6845594e77542f712e34a7c77eead1aadb38e5a9
avish1990 <avish1990@gmail.com>
2018-04-12 11:16:13 +0530

    Update version.sh

--- sam-ssm/version.sh
+++ sam-ssm/version.sh
@@ -1,3 +1,4 @@
+# Script to create aliases for previous version of Lambda.
 #!/bin/bash
 
 version=`aws lambda get-alias --name prod --function-name SSM | grep FunctionVersion | sed -e 's/\"//g' |  awk '{print$2}' | sed -e 's/,//g'`
